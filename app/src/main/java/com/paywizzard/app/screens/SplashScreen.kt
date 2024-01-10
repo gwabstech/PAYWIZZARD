33d537b125d767c68d3198d38041649082a2e25a
Gwabstech <gwabstech@gmail.com>
2024-01-11 00:26:08 +0100

    Finished Splash Screen

---
+++ app/src/main/java/com/paywizzard/app/screens/SplashScreen.kt
@@ -0,0 +1,114 @@
+package com.paywizzard.app.screens
+
+import android.widget.Button
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.paywizzard.app.R
+import com.paywizzard.app.ui.theme.PAYWIZZARDTheme
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+) {
+    val airforce_blue = Color(0xFF0010c2)
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+
+
+        Spacer(modifier = modifier.height(10.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f), // Give more space to the top section
+            contentAlignment = Alignment.Center
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "",
+                modifier = modifier
+                    .width(100.dp)
+                    .height(90.dp) // Adjust size as needed
+            )
+        }
+        // Bottom section with image and button
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 10.dp), // Add some padding around the elements
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+/*
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    modifier = modifier.size(60.dp) // Adjust size as needed
+                )
+                Spacer(modifier = modifier.width(2.dp))
+
+ */
+               Text(
+                   text = stringResource(R.string.appName),
+                   fontSize = 20.sp,
+                   fontWeight = FontWeight.Bold,
+                   color = airforce_blue,
+                   letterSpacing = 2.sp
+
+               )
+            }
+
+            Text(
+                text = stringResource(R.string.appVersion),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = modifier.padding(start = 10.dp)
+            )
+            Spacer(modifier = modifier.height(10.dp))
+
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true,)
+@Composable
+fun SplashScreenPreview() {
+    PAYWIZZARDTheme {
+        SplashScreen()
+    }
+}
